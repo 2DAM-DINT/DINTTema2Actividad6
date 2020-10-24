b35f62ef264f39175b3e33b0fb08567ffd92e112
adrianCG <adrianCG@mesi>
2020-10-24 10:56:07 +0200

    Agregar archivos de proyecto.

---
+++ Actividad6/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Actividad6
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void imagen_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 1;
+        }
+
+        private void imagen_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 0.5;
+        }
+    }
+}
